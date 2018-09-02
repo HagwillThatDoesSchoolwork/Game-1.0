bc58e85218a9f861f0f79295ae3010cbf9d6966a
Jakob Bulow <jakobbulow01@gmail.com>
2018-09-02 22:13:08 +0200

    Fiende och FärgSwitch
    Fiende och Färgswitch, done kinda

---
+++ Game 1.0/Assets/Scripts/Fiende.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fiende : MonoBehaviour {
+
+    private int x;
+
+    private void Start()
+    {
+        int Sdirection = Random.Range(-1, 1);
+
+        if (Sdirection < 0)
+            x = 1;
+        else
+            x = -1;       
+    }
+
+    private void Update()
+    {
+        Move(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (x == 1)
+            x = -1;
+        else if (x == -1)
+            x = 1;
+    }
+
+    public void Move(GameObject Fiende, float speed = 3)
+    {     
+        Vector3 direction = new Vector3(x, 0, 0); 
+        direction = direction.normalized * speed; 
+        Fiende.GetComponent<Rigidbody2D>().velocity = direction;
+    }
+}
---
+++ Game 1.0/Assets/Scripts/FärgSwitchColl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FärgSwitchColl : MonoBehaviour {
+   
+    //Välj antalfärger i inspector
+    public Color[] DMGcollors;
+    private float colortime = 1f;
+    private bool dmg = false;
+    
+    private void Update()
+    {
+        if (dmg)
+        {
+            colortime -= Time.deltaTime;
+            if (colortime <= 0)
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.white;
+                colortime = 1;
+                dmg = false;
+            }
+        }       
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Fiende")
+        {
+            if (gameObject.tag != "Fiende")
+            {
+                gameObject.GetComponent<Renderer>().material.color = DMGcollors[Random.Range(0, DMGcollors.Length)];
+                dmg = true;
+            }
+        }
+        else if (coll.gameObject.tag == "Main")
+        {
+            gameObject.GetComponent<Renderer>().material.color = DMGcollors[Random.Range(0, DMGcollors.Length)];
+            dmg = true;
+        }
+    }
+}
